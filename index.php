2fe807b594c0dc5abd4e23d75c100bcb236ecf91
esteban1234 <tva_libra@hotmail.com>
2017-06-27 11:07:19 -0500

    inicio del index

---
+++ index.php
@@ -0,0 +1,45 @@
+
+<?php include 'vistas/encabezado.php' ?>
+<body>
+
+  <header>
+    <figure class="logo pull-left">
+      <img src="img/pecydem.png" alt="" class="img-responsive">
+    </figure>
+
+    <div class="menu pull-right">
+    <a href="" class="pull-left botonarriba">CONTACTANOS</a>
+     <div class="bars pull-right">
+      <a href=""> MENU <i class="fa fa-bars" aria-hidden="true"></i></a>
+     </div>
+    </div>
+
+ <!--    <nav>
+      <ul>
+        <li><a href="">INICIO</a></li>
+        <li><a href="">NOSOTROS</a></li>
+        <li><a href="">SERVICIOS</a></li>
+        <li><a href="">CONTACTO</a></li>
+      </ul>
+    </nav> -->
+  </header>
+  <div class="linea">
+    
+  </div>
+  <div class="banner">
+    <div class="fondo">
+      <h1 style="color: #811B31; text-align: center; font-size: 100px; margin-top: 7%;">PECYDEM</h1>
+      <h2 style="color: #000; text-align: center; margin-top: 1%; font-weight: bold;">Construya mejor rentabilidad, sin la presión.</h2>
+      <h3 style="color: #000; text-align: center; width: 50%; margin: 0 auto; margin-top: 2%; font-weight: bold;">Los líderes actuales de finanzas están volteando hacia PECYDEM para controlar costos y flujos de efectivo asociados con gastos y facturas.</h3>
+    </div>
+  </div>
+
+
+
+<span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>
+<?php include 'vistas/pie.php' ?>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/botonarriba.js"></script>
+</body>
+</html>
---
+++ vistas/pie.php
@@ -0,0 +1,3 @@
+<footer>
+<p>Derechos de autor 2017 © | PECYDEM DISEÑO Y ADMINISTRACION S. de R.L de C.V.</p>
+</footer>
